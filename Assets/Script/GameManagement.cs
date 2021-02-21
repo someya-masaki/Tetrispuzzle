cb8b721afc10dd526e9cc22c4adfdd3a84a6941d
somemasa0430 <somemasa0430@docomo.ne.jp>
2021-02-21 13:19:31 +0900

    未完成、不具合あり

---
+++ Assets/Script/GameManagement.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManagement : MonoBehaviour
+{
+    // スコア関連
+    public Text scoreText;
+    private int score;
+    public int currentScore;
+    public int clearScore = 1000;
+
+    // タイマー関連
+    public Text timerText;
+    public float gameTime = 180.0f;
+    int seconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initialize();
+    }
+    void Update()
+    {
+        TimeManagement();
+    }
+
+    private void TimeManagement()
+    {
+        gameTime -= Time.deltaTime;
+        seconds = (int)gameTime;
+        timerText.text = seconds.ToString();
+
+        if(seconds == 0)
+        {
+            GameOver();
+        }
+    }
+
+    // ゲーム開始前の状態に戻す
+    private void Initialize()
+    {
+        // スコアを0に戻す
+        score = 0;
+    }
+
+    // スコアの追加
+    public void AddScore()
+    {
+        score += 100;
+        currentScore += score;
+        scoreText.text = "Score: " + currentScore.ToString();
+        if(currentScore >= clearScore)
+        {
+            GameClear();
+        }
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GameClear()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Script/Mino.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mino : MonoBehaviour
+{
+    public float previousTime;
+    // minoが落ちるタイム
+    public float fallTime = 1f;
+    // ステージの大きさ
+    private static int width = 10;
+    private static int height = 20;
+    // mino回転
+    public Vector3 rotationPoint;
+
+    private static Transform[,] grid = new Transform[width, height];
+    // Update is called once per frame
+    void Update()
+    {
+        MinoMovement();
+    }
+
+    private void MinoMovement()
+    {
+        // Aキーで左に動く
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            if(!ValidMovement())
+            {
+                transform.position -= new Vector3(-1, 0, 0);
+            }
+        }
+        // Dキーで右に動く
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+            if (!ValidMovement())
+            {
+                transform.position -= new Vector3(1, 0, 0);
+            }
+        }
+        // 自動で下に移動させつつ、Sキーでも移動する
+        else if (Input.GetKey(KeyCode.S) || Time.time - previousTime >= fallTime) 
+        {
+            transform.position += new Vector3(0, -1, 0);
+            if (!ValidMovement())
+            {
+                transform.position -= new Vector3(0, -1, 0);
+                AddToGrid();
+                CheckLines();
+                this.enabled = false;
+                FindObjectOfType<SpawnMino>().NewMino();
+            }
+            previousTime = Time.time;
+        }
+        else if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // ブロックの回転
+            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
+        }
+    }
+
+    // ラインがあるか確認
+    public void CheckLines()
+    {
+        for(int i = height - 1; i >= 0; i--)
+        {
+            if(HasLine(i))
+            {
+                DeleteLine(i);
+                RowDown(i);
+            }
+        }
+    }
+    // 列がそろっているか確認
+    bool HasLine(int i)
+    {
+        for(int j = 0; j < width;j++)
+        {
+            if (grid[j, i] == null)
+                return false;
+        }
+        FindObjectOfType<GameManagement>().AddScore();
+        return true;
+    }
+    // ラインを消す
+    void DeleteLine(int i)
+    {
+        for(int j = 0;j < width;j++)
+        {
+            Destroy(grid[j, i].gameObject);
+            grid[j, i] = null;
+        }
+    }
+    // 列を下げる
+    public void RowDown(int i)
+    {
+        for(int y = i;y < height;y++)
+        {
+            for(int j = 0; j < width;j++)
+            {
+                if(grid[j,y] != null)
+                {
+                    grid[j, y - 1] = grid[j, y];
+                    grid[j, y] = null;
+                    grid[j, y - 1].transform.position -= new Vector3(0, 1, 0);
+                }
+            }
+        }
+    }
+
+    void AddToGrid()
+    {
+        foreach(Transform children in transform)
+        {
+            int roundX = Mathf.RoundToInt(children.transform.position.x);
+            int roundY = Mathf.RoundToInt(children.transform.position.y);
+            grid[roundX, roundY] = children;
+
+            // height-1 = 19のところまでブロックがきたらGameOver
+            if(roundX >= height -1)
+            {
+                // GameOverメソッドを呼び出す
+                FindObjectOfType<GameManagement>().GameOver();
+            }
+        }
+    }
+
+    // minoの移動範囲の制御
+    bool ValidMovement()
+    {
+        foreach(Transform children in transform)
+        {
+            int roundX = Mathf.RoundToInt(children.transform.position.x);
+            int roundY = Mathf.RoundToInt(children.transform.position.y);
+            // minoがステージよりはみ出さないように制御
+            if(roundX < 0 || roundX >= width || roundY < 0 || roundY >= height)
+            {
+                return false;
+            }
+            if(grid[roundX,roundY]!= null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
